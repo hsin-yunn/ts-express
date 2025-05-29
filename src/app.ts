57ea42ca35bc3d190fe12f7852d453c50216af44
曹馨云 <caoxinyun@caoxinyundeMacBook-Pro.local>
2025-05-29 23:24:51 +0800

    fix

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,12 @@ import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
+import cors from "cors";
 
 // import indexRouter from "routes/index";
 // import usersRouter from "routes/users";
 
 const app = express();
-const cors = require('cors');
 
 app.use(logger("dev"));
 app.use(express.json());
